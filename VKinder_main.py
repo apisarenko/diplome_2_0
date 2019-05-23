40f2fa42b30c6e0b27ee728383854648134958c7
apisarenko <47703396+apisarenko@users.noreply.github.com>
2019-05-23 15:59:23 +0300

    Update VKinder_main.py

--- VKinder_main.py
+++ VKinder_main.py
@@ -1,7 +1,8 @@
-from modules.vk_auth import  VKAuth
-from modules.friend import FriendSearch
+from modules.vk_auth import VKAuth
 from modules.db_file import DB_Mongo
+from modules.user import UserVK
 from modules.user import User
+from modules.user import FriendSearch
 from pprint import pprint
 import json
 import codecs
@@ -22,30 +23,6 @@ def get_potencial_friend_city_sex(friends, user_data):
         path_user_data['bdate'] = input('Введите дату рождения в формате "D.M.YYYY": ')
     potencial_friend_city_sex_out = friends.get_partners_by_basic(friend_sex, path_user_data['city']['id'])['response']
     return potencial_friend_city_sex_out
-
-
-def db_operation(db_in, partners_basic_in, user_data, friends, user_id):
-    for item in partners_basic_in['list_friend']:
-        db_in.import_data(item)
-    print('Формируем список потенциальных друзей для {}'.format((user_data['response'][0]['first_name']))
-          + ' ' + (user_data['response'][0]['last_name']))
-    db_in.put_fields()  # создаем дополнительные поля
-    basic_id = db_in.get_basic_id()
-    print('Получили список {} потенциальных друзей'.format(len(basic_id)))
-    max = input('Введите насколько лет друг может быть старше: ')
-    min = input('Введите насколько лет друг может быть младше: ')
-    db_in.put_value_bdate(user_data['response'][0]['bdate'], max, min)  # отметили в БД людей по возрасту
-    record_number = 0
-    for id in basic_id:
-        print('Поиск общих друзей и общих групп потенциального друга №: {}'.format(record_number + 1))
-        if friends.get_com_friends(user_id, id) > 1:  # отметили в БД людей с общими друзьями
-             db_in.put_value_com(id)
-        if friends.get_com_groups(user_id, id) > 1:  # отметили в БД людей с общими группами
-            db_in.put_value_com_2(id)
-        record_number += 1
-    if 'interests' in user_data['response'][0].keys():
-        for part in user_data['response'][0]['interests'].split():  # отметили людей по общим интересам
-            db_in.put_value_inter(part)
 
 
 def sort_potencial_friend():
@@ -74,7 +51,7 @@ def main():
     db = DB_Mongo()
     db.all_drop()
     potencial_friend_city_sex = get_potencial_friend_city_sex(friends, user_data)  # Получаем список из VK
-    db_operation(db, potencial_friend_city_sex, user_data, friends, user_id)  # запись базовый список в БД
+    db.db_operation(db, potencial_friend_city_sex, user_data, friends, user_id)  # запись базовый список в БД
     selection_criterion = sort_potencial_friend()  # формируем уточняющие критерии
     out_db = db.find_n_drop_adv(selection_criterion)  # сортируем по уточняющим критериям
     out_list = []
